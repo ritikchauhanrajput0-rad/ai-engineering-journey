6f62e72497a3baf447e64e5d60331c0f3584ce02
ritikchauhanrajput0-rad <ritikchauhanrajput0@gmail.com>
2026-07-07 13:44:54 +0530

    Week 1 Day 1: Setup project and first Groq LLM app

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+myapi_key=os.getenv("GROQ_API_KEY")
+if not myapi_key:
+    raise ValueError("NO ITEM FOUND")
+
+client =Groq(api_key=myapi_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="do you know hardik pandya"
+
+message={
+    "role":role,
+    "content":prompt
+}
+messages=[message]
+response = client.chat.completions.create(model=model,messages=messages)
+print(response)
+print (333333333333333333333333333333333333)
+answer=response.choices[0].message.content
+print(answer)
+    
